1ce1c9e8cd8ba0679fa4353eddd0fe52c9071872
EnzoDiazDev <enzodiazdev@gmail.com>
2021-10-28 20:50:28 -0300

    basic feature

--- src/database/Repository.ts
+++ src/database/Repository.ts
@@ -1,6 +1,7 @@
 export default interface Repository<Entity> {
+    id:string;
     get_all(): Promise<Entity[]>;
-    get(id:string): Promise<Entity>;
+    get(id:string): Promise<Entity|null>;
     create(entity:Entity): Promise<void>;
     update(entity:Entity): Promise<void>;
     delete(id:string): Promise<void>;
